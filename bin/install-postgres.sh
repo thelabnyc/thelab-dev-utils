886fd32d46001df0c63c5378e6283354c0870067
Craig Weber <crgwbr@gmail.com>
2026-05-12 18:15:04 -0400

    chore(deps): update ubuntu docker tag to v26

--- bin/install-postgres.sh
+++ bin/install-postgres.sh
@@ -2,8 +2,10 @@
 
 set -euxo pipefail
 
-wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
-add-apt-repository "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main"
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc \
+    | gpg --dearmor -o /etc/apt/keyrings/pgdg.gpg
+echo "deb [signed-by=/etc/apt/keyrings/pgdg.gpg] http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" \
+    > /etc/apt/sources.list.d/pgdg.list
 
 apt-get update
 apt-get install -y \
